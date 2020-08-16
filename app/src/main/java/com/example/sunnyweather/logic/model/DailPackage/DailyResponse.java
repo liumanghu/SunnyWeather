2febbbac003fb6cb573b3c355a33eb4361ebf164
husheng <1246068777@qq.com>
2020-08-16 20:59:54 +0800

    Second commit

---
+++ app/src/main/java/com/example/sunnyweather/logic/model/DailPackage/DailyResponse.java
@@ -0,0 +1,4 @@
+package com.example.sunnyweather.logic.model.DailPackage;
+
+public class DailyResponse {
+}
---
+++ app/src/main/java/com/example/sunnyweather/logic/model/RealtimePackage/Realtime.java
@@ -0,0 +1,4 @@
+package com.example.sunnyweather.logic.model.RealtimePackage;
+
+public class Realtime {
+}
